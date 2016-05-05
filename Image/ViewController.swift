539640aab88d9163c09b932d5360e7f32062f7ac
Snigdhaman Chatterjee <snigdhaman1991@gmail.com>
2016-05-05 20:09:39 +0530

    NSURL demo app
    App to demo NSURL

--- Image/ViewController.swift
+++ Image/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "https://upload.wikimedia.org/wikipedia/en/9/93/FF_XV_Demo_screenshot.png")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) -> Void in
+            
+            if error != nil {
+                
+                print("Error")
+                
+            } else {
+                
+                var picDirectory:String?
+                
+                var paths: [AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+                if paths.count > 0 {
+                    
+                    picDirectory = paths[0] as? String
+                    
+                    let savePath = picDirectory! + "/img.jpg"
+                    
+                    NSFileManager.defaultManager().createFileAtPath(savePath, contents: data, attributes: nil)
+                    
+                    
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            
+                            self.imageView.image = UIImage(named: savePath)
+                        
+                    })
+                    
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
